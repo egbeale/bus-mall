1022b8bde0342c5475305c4bef8ce283c441dce8
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-03-26 17:18:55 -0700

    implemented local storage

--- js/app.js
+++ js/app.js
@@ -5,9 +5,7 @@
 let products = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
 let productArray = [];
 let votingRounds = 25;
-let productVotes = [];
-let productViews = [];
-let yetAnotherIndex = [];
+let indexNew = [];
 
 // ***************************** DOM REFERENCES ***************************
 
@@ -15,15 +13,32 @@ let imgContainer = document.getElementById('img-container');
 let img1 = document.getElementById('img-1');
 let img2 = document.getElementById('img-2');
 let img3 = document.getElementById('img-3');
-
+// let productVotes = [];
+// let productViews = [];
+// let productNames = [];
 let resultsBtn = document.getElementById('show-results-btn');
+
 
 // ********************** CANVAS ELEMENT FOR CHART ********************
 
 let ctx = document.getElementById('my-chart').getContext('2d');
 
+
+
+//*************** LOCAL STORAGE ****************
+
+let getArr = localStorage.getItem('products');
+console.log(getArr);
+
+let parsedArr = JSON.parse(getArr);
+console.log(parsedArr);
+
+
+
+
 // ************************* CONSTRUCTOR *****************************
 // fileExtension = default parameter. assigning value of jpg
+
 function Product(name, fileExtension = 'jpg') {
   this.productName = name;
   this.image = `images/${name}.${fileExtension}`;
@@ -33,46 +48,69 @@ function Product(name, fileExtension = 'jpg') {
   productArray.push(this);
 }
 
-for(let i = 0; i < products.length; i++){
-  if(products[i] === 'sweep') {
-    new Product('sweep', 'png');
-  } else {
-    new Product(`${products[i]}`);
-  }
+if(getArr){
+  productArray = parsedArr;
+} else {
+  new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('breakfast');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
+new Product('sweep', 'png');
+new Product('tauntaun');
+new Product('unicorn');
+new Product('water-can');
+new Product('wine-glass');
 }
+
+// for(let i = 0; i < products.length; i++){
+//   if(products[i] === 'sweep') {
+//     new Product('sweep', 'png');
+//   } else {
+//     new Product(`${products[i]}`);
+//   }
+// }
 
 
 // ********************** HELPER FUNCTIONS **************************
 function getRandomIndex(){
   return Math.floor(Math.random() * productArray.length);
-  // from w3resource.com
 }
 
 function renderImages(){
 
-  while (yetAnotherIndex.length < 6) {
+  while (indexNew.length < 6) {
     let imgIndex = getRandomIndex();
-    if (!yetAnotherIndex.includes(imgIndex)) {
-      yetAnotherIndex.push(imgIndex);
+    if (!indexNew.includes(imgIndex)) {
+      indexNew.push(imgIndex);
     }
   }
 
-  if(yetAnotherIndex.length === 6) {
-    yetAnotherIndex.splice(0, 3);
+  if(indexNew.length === 6) {
+    indexNew.splice(0, 3);
   }
 
   //**** RENDER IMGS ON SCREEN **** */
-  img1.src = productArray[yetAnotherIndex[0]].image;
-  img1.alt = productArray[yetAnotherIndex[0]].productName;
-  productArray[yetAnotherIndex[0]].views++;
+  img1.src = productArray[indexNew[0]].image;
+  img1.alt = productArray[indexNew[0]].productName;
+  productArray[indexNew[0]].views++;
 
-  img2.src = productArray[yetAnotherIndex[1]].image;
-  img2.alt = productArray[yetAnotherIndex[1]].productName;
-  productArray[yetAnotherIndex[1]].views++;
+  img2.src = productArray[indexNew[1]].image;
+  img2.alt = productArray[indexNew[1]].productName;
+  productArray[indexNew[1]].views++;
 
-  img3.src = productArray[yetAnotherIndex[2]].image;
-  img3.alt = productArray[yetAnotherIndex[2]].productName;
-  productArray[yetAnotherIndex[2]].views++;
+  img3.src = productArray[indexNew[2]].image;
+  img3.alt = productArray[indexNew[2]].productName;
+  productArray[indexNew[2]].views++;
 
 }
 
@@ -83,7 +121,7 @@ renderImages();
 function handleClick(event){
   let imgClicked =  event.target.alt;
 
-  for(let i = 0; i < productArray.length; i++){
+  for (let i = 0; i < productArray.length; i++){
     if (imgClicked === productArray[i].productName){
       productArray[i].clicks++;
     }
@@ -92,16 +130,25 @@ function handleClick(event){
   votingRounds--;
   if(votingRounds === 0){
     imgContainer.removeEventListener('click', handleClick);
-    return;
+
+    let strArr = JSON.stringify(productArray);
+    localStorage.setItem('product', strArr);
+    // return;
   }
   renderImages();
 }
 
 function handleShowResults(){
 
+  let productVotes = [];
+  let productViews = [];
+  let productNames = [];
+
+
   for(let i = 0; i < productArray.length; i++) {
     productVotes.push(productArray[i].clicks);
     productViews.push(productArray[i].views);
+    productNames.push(productArray[i].name);
   }
 
   if (votingRounds === 0) {
@@ -139,8 +186,6 @@ function handleShowResults(){
         }
       }
     };
-    // call the constructor function, and what the chart.js told us is that we need to pass it two arguments. pass it that canvas element, and the actual argument.
-    // We're allowed to do this bc
     new Chart(ctx, myChartObj);
   }
 }
